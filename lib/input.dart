34988894709293533f54b94cc7c7cc36f2d65493
t-midorikawa <t-midorikawa@kyouikusangyou.co.jp>
2021-08-07 16:28:42 +0900

    20210807

--- lib/input.dart
+++ lib/input.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class PageInput extends StatelessWidget {
+enum RadioValue { SHISYUTSU, SYUNYU }
+
+class PageInput extends StatefulWidget {
+  @override
+  _PageInputState createState() => _PageInputState();
+}
+
+class _PageInputState extends State<PageInput> {
+  RadioValue _gValue = RadioValue.SHISYUTSU;
+  DateTime _date = new DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,19 +19,61 @@ class PageInput extends StatelessWidget {
         title: Text('PageInput'),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              TextButton(
-                onPressed: () {},
-                child: Text('入力'),
-              )
-            ],
+      body: Column(children: <Widget>[
+        Row(children: <Widget>[
+          RadioListTile(
+            title: Text('支出'),
+            value: RadioValue.SHISYUTSU,
+            groupValue: _gValue,
+            onChanged: (value) => _onRadioSelected(value),
           ),
-        ),
-      ),
+          RadioListTile(
+            title: Text('収入'),
+            value: RadioValue.SYUNYU,
+            groupValue: _gValue,
+            onChanged: (value) => _onRadioSelected(value),
+          ),
+        ]),
+        Row(children: <Widget>[
+          new Text("日付"),
+          new Text(_date.toString()),
+          IconButton(
+            icon: Icon(Icons.date_range),
+            onPressed: () => _selectDate(context),
+          )
+        ]),
+        Row(children: <Widget>[
+          new Text("金額"),
+          new TextField(),
+        ]),
+        Row(children: <Widget>[
+          new Text("項目名"),
+          new TextField(),
+        ]),
+        Row(children: <Widget>[
+          new Text("分類"),
+          new TextField(),
+        ]),
+      ]),
     );
+  }
+
+  void _onRadioSelected(value) {
+    setState(() {
+      _gValue = value;
+    });
+  }
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime.now().add(new Duration(days: 360)));
+
+    if (picked != null) {
+      // 日時反映
+      setState(() => _date = picked);
+    }
   }
 }
