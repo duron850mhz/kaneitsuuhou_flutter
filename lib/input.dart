cf957e1bea8837acdb07da57ff61eee99a5bdc67
t-midorikawa <t-midorikawa@kyouikusangyou.co.jp>
2021-08-17 22:37:54 +0900

    combobox

--- lib/input.dart
+++ lib/input.dart
@@ -12,8 +12,16 @@ class _PageInputState extends State<PageInput> {
   RadioValue _gValue = RadioValue.SHISYUTSU;
   DateTime _date = new DateTime.now();
   DateFormat df = DateFormat("yyyy/MM/dd");
+  List<DropdownMenuItem<int>> _items = [];
+  int _selectItem = 0;
 
   @override
+  void initState() {
+    super.initState();
+    _setComboItems();
+    _selectItem = _items[0].value!;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -46,7 +54,17 @@ class _PageInputState extends State<PageInput> {
             onPressed: () => _selectDate(context),
           )
         ]),
+        DropdownButton(
+          items: _items,
+          value: _selectItem,
+          onChanged: (int? value) => {
+            setState(() {
+              _selectItem = value!;
+            }),
+          },
+        ),
         TextField(
+          keyboardType: TextInputType.number,
           decoration: const InputDecoration(
             icon: Icon(Icons.money_off_csred_rounded),
             labelText: '金額',
@@ -83,5 +101,21 @@ class _PageInputState extends State<PageInput> {
       // 日時反映
       setState(() => _date = picked);
     }
+  }
+
+  void _setComboItems() {
+    _items
+      ..add(DropdownMenuItem(
+        child: Text('A'),
+        value: 1,
+      ))
+      ..add(DropdownMenuItem(
+        child: Text('B'),
+        value: 2,
+      ))
+      ..add(DropdownMenuItem(
+        child: Text('C'),
+        value: 3,
+      ));
   }
 }
